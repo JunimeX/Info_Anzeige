ec8fcff47f127012724906d97b44c9491cef85d5
Justin Mergenthaler <jutinmergentahler@gmail.com>
2024-02-22 12:53:03 +0100

    Annäherung Login Fenster und erstellung erster Klassen.

---
+++ Info_Anzeige/Klassen/AnzeigeContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Info_Anzeige.Klassen
+{
+    internal class AnzeigeContext : DbContext
+    {
+        public DbSet<Benutzer> Benutzers { get; set; }
+    }
+}
---
+++ Info_Anzeige/Klassen/Benutzer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Info_Anzeige.Klassen
+{
+    internal class Benutzer
+    {
+        [Key]
+        private int benutzererid;
+        public int Benutzererid 
+        {  
+            get { return benutzererid;}
+            set {  benutzererid = value; }
+        }
+
+        private int name;
+        public int Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int berechtigung;
+        public int Berechtigung        
+        {
+            get { return berechtigung; }
+            set { berechtigung = value; }
+        }
+
+        private string passwort;
+        public string Passwort
+        {
+            get { return passwort;}
+            set { passwort = value; }
+        }        
+    }
+}
