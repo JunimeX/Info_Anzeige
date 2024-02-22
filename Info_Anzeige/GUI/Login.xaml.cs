a55166270dda9e85381701958d5ed3eb0105c63c
JunimeX <137988985+JunimeX@users.noreply.github.com>
2024-02-22 13:05:43 +0100

    Annäherung Login Fenster und erstellung erster Klassen. (#2)
    Co-authored-by: Justin Mergenthaler <jutinmergentahler@gmail.com>

--- Info_Anzeige/GUI/Login.xaml.cs
+++ Info_Anzeige/GUI/Login.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Info_Anzeige.GUI;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Info_Anzeige
+namespace Info_Anzeige.GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Page
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-            MainFrame.NavigationService.Navigate(new Login());
         }
     }
 }
